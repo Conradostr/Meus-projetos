b99b1dfa6dd526d2a7602722884c5270fa215825
Caio Conrado <caioconr63@gmail.com>
2023-08-08 20:20:17 -0300

    second commit

--- js/index.js
+++ js/index.js
@@ -4,9 +4,9 @@ import { Router } from "./router.js"
 const router = new Router()
 
 router.add("/", "./pages/home.html")
-router.add("/ideias", "./pages/ideias.html")
-router.add("/membros", "./pages/membros.html")
-router.add("/discord", "./pages/discord.html")
+router.add("/ideias", "/pages/ideias.html")
+router.add("/membros", "/pages/membros.html")
+router.add("/discord", "/pages/discord.html")
 
 
 
